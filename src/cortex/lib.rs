17c8f26e2d7a6b94cccafadd7c3a00e71ee90233
Ben Harris <mail@bharr.is>
2014-01-14 19:47:37 +0800

    update

---
+++ src/cortex/lib.rs
@@ -0,0 +1,8 @@
+#[no_std];
+#[crate_id = "cortex#0.1"];
+#[crate_type = "lib"];
+#[license = "MIT"];
+
+#[feature(asm)];
+
+pub mod regs;
---
+++ src/cortex/regs.rs
@@ -0,0 +1,36 @@
+// load and store. word, half, byte. normal and wait (dmb) and wait_all (dsb)
+
+fn store_word(addr: u32, val: u32) {
+  unsafe {
+    asm!("str $0, [$1]"
+      :
+      : "r"(val), "r"(addr)
+      :
+      : "volatile"
+      );
+  }
+}
+
+fn store_word_wait(addr: u32, val: u32) {
+  unsafe {
+    asm!("str $0, [$1]\n\t
+          dmb"
+      :
+      : "r"(val), "r"(addr)
+      :
+      : "volatile"
+      );
+  }
+}
+
+fn store_word_wait_all(addr: u32, val: u32) {
+  unsafe {
+    asm!("str $0, [$1]\n\t
+          dsb"
+      :
+      : "r"(val), "r"(addr)
+      :
+      : "volatile"
+      );
+  }
+}
