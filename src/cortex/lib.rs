87d3e301e0c47dcf2bcfffbc8e62685ee328fc11
Ben Harris <mail@bharr.is>
2014-01-21 16:44:26 +0800

    working example

--- src/cortex/lib.rs
+++ src/cortex/lib.rs
@@ -1,3 +1,6 @@
+#[feature(managed_boxes, globs, macro_registrar, macro_rules)];
+#[macro_escape];
+
 #[no_std];
 #[crate_id = "cortex#0.1"];
 #[crate_type = "lib"];
@@ -6,3 +9,17 @@
 #[feature(asm)];
 
 pub mod regs;
+
+#[macro_export]
+macro_rules! sfr_store_word2(
+  ($addr:ident, $val:ident) => (
+    unsafe {
+      asm!("str $0, [$1]"
+           :
+           : "r"($val), "r"($addr)
+           :
+           : "volatile"
+           );
+    }
+  );
+)
--- src/cortex/regs.rs
+++ src/cortex/regs.rs
@@ -1,36 +1,54 @@
+#[macro_escape];
 // load and store. word, half, byte. normal and wait (dmb) and wait_all (dsb)
 
-pub fn store_word(addr: u32, val: u32) {
-  unsafe {
-    asm!("str $0, [$1]"
-      :
-      : "r"(val), "r"(addr)
-      :
-      : "volatile"
-      );
-  }
-}
+#[macro_export]
+macro_rules! sfr_store_word(
+  ($addr:ident, $val:ident) => (
+    unsafe {
+      asm!("str $0, [$1]"
+           :
+           : "r"($val), "r"($addr)
+           :
+           : "volatile"
+           );
+    }
+  );
+)
 
-pub fn store_word_wait(addr: u32, val: u32) {
-  unsafe {
-    asm!("str $0, [$1]\n\t
-          dmb"
-      :
-      : "r"(val), "r"(addr)
-      :
-      : "volatile"
-      );
-  }
-}
-
-pub fn store_word_wait_all(addr: u32, val: u32) {
-  unsafe {
-    asm!("str $0, [$1]\n\t
-          dsb"
-      :
-      : "r"(val), "r"(addr)
-      :
-      : "volatile"
-      );
-  }
-}
+// #[allow(dead_code)]
+// pub fn store_word(addr: u32, val: u32) {
+//   unsafe {
+//     asm!("str $0, [$1]"
+//       :
+//       : "r"(val), "r"(addr)
+//       :
+//       : "volatile"
+//       );
+//   }
+// }
+// 
+// #[allow(dead_code)]
+// pub fn store_word_wait(addr: u32, val: u32) {
+//   unsafe {
+//     asm!("str $0, [$1]\n\t
+//           dmb"
+//       :
+//       : "r"(val), "r"(addr)
+//       :
+//       : "volatile"
+//       );
+//   }
+// }
+// 
+// #[allow(dead_code)]
+// pub fn store_word_wait_all(addr: u32, val: u32) {
+//   unsafe {
+//     asm!("str $0, [$1]\n\t
+//           dsb"
+//       :
+//       : "r"(val), "r"(addr)
+//       :
+//       : "volatile"
+//       );
+//   }
+// }
