9b4676adb4eb9b05472ba1923fd1679578286513
Ben Harris <mail@bharr.is>
2014-05-02 22:08:41 +0800

    PIC mode and STACK_LIMIT symbol

--- examples/blink/main.rs
+++ examples/blink/main.rs
@@ -8,8 +8,49 @@
 extern crate cortex;
 //extern crate freescale;
 
+use cortex::regs::set;
+
+//#[link_section=".isr_vector_temp"]
+//#[no_mangle]
+//pub static ISRVectors: [extern unsafe fn(), ..4] = [
+//      _stack_base,
+//      main,
+//      isr_nmi,
+//      isr_hardfault,
+//];
+
+extern {
+    static mut __STACK_LIMIT: u32;
+    static __StackLimit: u32;
+}
+//
+//#[no_mangle]
+//pub static mut STACK_LIMIT: u32 = __StackLimit;
+
+//#[link_section=".test"]
+//#[no_mangle]
+//pub static FlashConfig: [u8, ..16] = [
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//      0xFE,
+//      0xFF,
+//      0xFF,
+//      0xFF,
+//];
+
 #[no_mangle]
 pub extern "C" fn _start() {
+    unsafe { __STACK_LIMIT = (&__StackLimit as *u32) as u32; }
     loop { }
 }
 
